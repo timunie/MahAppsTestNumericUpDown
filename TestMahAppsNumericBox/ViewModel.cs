886d641aeb8e6c90a78eff1f9547ecf5b3c71e56
timunie <47110241+timunie@users.noreply.github.com>
2019-11-01 14:09:20 +0100

    Proposal

--- TestMahAppsNumericBox/ViewModel.cs
+++ TestMahAppsNumericBox/ViewModel.cs
@@ -5,6 +5,8 @@
 
     public class ViewModel : INotifyPropertyChanged
     {
+        static double _default = 5;
+
         private double _testValue;
 
         public double TestValue
@@ -12,15 +14,31 @@
             get => _testValue;
             set
             {
-                if (Equals(value, _testValue)) return;
                 _testValue = value;
-                OnPropertyChanged();
+                OnPropertyChanged2(null);// This updates all Properties
+            }
+        }
+
+        public double? NullableTestValue
+        {
+            get => _testValue;
+            set
+            {
+                _testValue = value ?? _default;
+                OnPropertyChanged2(null); // This updates all Properties
             }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        // OnPropertyChanged with option to call for more than one property
+        protected virtual void OnPropertyChanged2(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
